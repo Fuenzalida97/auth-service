a9b866e40cb1de18b4bb4f04cb2dd4f2e02f5515
MiguelFuenzalida <miguelfuenzalida.n@gmail.com>
2025-08-05 22:10:32 -0400

    Se cambia prettier a LF

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,9 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'off',
       'no-unused-vars': 'off',
       'no-console': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
     },
   },
 );
